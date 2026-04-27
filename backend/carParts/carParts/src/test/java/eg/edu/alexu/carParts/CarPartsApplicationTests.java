e4b5a7f803695c2c09ee5293da00d588fd8ace25
huda gamal <hudagamalrahal@gmail.com>
2026-04-27 03:33:16 +0300

    created backend project

---
+++ backend/carParts/carParts/src/test/java/eg/edu/alexu/carParts/CarPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.carParts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
